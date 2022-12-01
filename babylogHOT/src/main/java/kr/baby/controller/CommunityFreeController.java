5c6f5a1ccd67bac0646abad2a9afa3980d849e3a
dwng3 <answer3dw@gmail.com>
2022-12-01 13:01:55 +0900

    Merge branch 'master' of https://github.com/HotKick/HotKick.git

--- babylogHOT/src/main/java/kr/baby/controller/CommunityFreeController.java
+++ babylogHOT/src/main/java/kr/baby/controller/CommunityFreeController.java
@@ -6,11 +6,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.baby.dao.BabyMyBatisDAO;
+
 public class CommunityFreeController implements Controller {
 
 	@Override
 	public String requestHandler(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		BabyMyBatisDAO dao = new BabyMyBatisDAO();
+		List<CommunityVO> list = dao.allList();
+		request.setAttribute("list", list);
+		
 		return "communityFree";
 	}
 
